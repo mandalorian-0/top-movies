e092d58a8e4a7a8c67ca2cac352160ac00535c54
Rodelin J Cesar <cesarrodelin@outlook.com>
2026-02-14 20:34:20 -0800

    feat: implement delete functionality

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import requests
-
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
 
 from db.__init__ import SessionLocal, create_tables
-from services.movie_service import get_all_movies, update_movie
+from services.movie_service import get_all_movies, update_movie, delete_movie
 from form.update import UpdateForm
 
 app = Flask(__name__)
@@ -28,6 +26,13 @@ def home():
     db = next(get_db())
     movies = get_all_movies(db)
 
+    if request.args:
+        try:
+            movie_id = request.args.get("id")
+            delete_movie(db, movie_id)
+            return redirect(url_for("home"))
+        except Exception as e:
+            print(e)
     return render_template("index.html", movies=movies)
 
 @app.route("/edit", methods=["GET", "POST"])
@@ -37,15 +42,12 @@ def edit():
     movie_id = request.args.get('id')
 
     if form.validate_on_submit():
-        # new_rating = form.data["rating"]
-        # new_review =form.data["review"]
 
         try:
             update_movie(db, movie_id, form.data)
             return redirect(url_for("home"))
         except Exception as e:
             print(e)
-
         
     return render_template("edit.html", form=form)
 
--- services/movie_service.py
+++ services/movie_service.py
@@ -7,15 +7,25 @@ def get_all_movies(db: Session) -> list[Movie]:
 def get_movie_by_id(db: Session, movie_id: int)-> Movie | None :
     return db.query(Movie).filter(Movie.id == movie_id).first()
 
-def update_movie(db: Session, id: int, movie_data: dict | None) -> Movie | None:
-    movie = get_movie_by_id(db, id)
+def update_movie(db: Session, movie_id: int, movie_data: dict | None) -> Movie | None:
+    movie_to_update = get_movie_by_id(db, movie_id)
 
-    if not movie:
+    if not movie_to_update:
         return None
 
-    movie.rating = movie_data["rating"]
-    movie.review = movie_data["review"]
+    movie_to_update.rating = movie_data["rating"]
+    movie_to_update.review = movie_data["review"]
 
     db.commit()
-    db.refresh(movie)
+    db.refresh(movie_to_update)
     # return movie
+
+def delete_movie(db: Session, movie_id: int):
+    movie_to_delete = get_movie_by_id(db, movie_id)
+
+    if not movie_to_delete:
+        return False
+
+    db.delete(movie_to_delete)
+    db.commit()
+    return True
