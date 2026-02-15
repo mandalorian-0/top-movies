2f60c8177a0e3f0cdb992cc9d27efb32df542d73
Rodelin J Cesar <cesarrodelin@outlook.com>
2026-02-14 18:14:06 -0800

    refactor: add static files to repo

--- main.py
+++ main.py
@@ -7,14 +7,22 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 from db.session import SessionLocal
+from services.movie_service import get_all_users
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.route("/")
 def home():
+    movies = get_all_users(get_db())
     return render_template("index.html")
 
 
